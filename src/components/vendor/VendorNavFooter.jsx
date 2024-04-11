4e6ed1e7348b356cbe6b401c39e8e481ccbc7d54
anickacodes <anickalewis@pursuit.org>
2024-04-11 00:15:39 -0400

    "filtered, analytics base, location button change"

--- src/components/vendor/VendorNavFooter.jsx
+++ src/components/vendor/VendorNavFooter.jsx
@@ -23,7 +23,7 @@ const VendorNavFooter = () => {
       setActiveIndex(0);
     } else if (pathname === '/vendormapview') {
       setActiveIndex(1);
-    } else if (pathname === '/analytics') {
+    } else if (pathname === '/vendoranalytics') {
       setActiveIndex(2);
     } else if (pathname === '/vendordashboard') {
       setActiveIndex(3);
@@ -50,7 +50,7 @@ const VendorNavFooter = () => {
           </Link>
         </li>
         <li className={`list ${activeIndex === 2 ? 'active' : ''}`} onClick={(e) => handleItemClick(2, e)}>
-          <Link to="/analytics">
+          <Link to="/vendoranalytics">
             <span className="icon">
               <i className="bi bi-stars"></i>
             </span>
